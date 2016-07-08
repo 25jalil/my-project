06ce7701090646903dd56199649a49f1268fa253
25jalil <25gaisarov@gmail.com>
2016-07-08 14:17:51 +0300

    tags finish

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :category
   has_many :tags_associations
   has_many :tags, through: :tags_associations
+
+  scope :posts_for_tag, -> (tag) do
+    includes(:tags_associations, :tags)
+   .joins(:tags)
+   .where("tags_associations.tag_id = ?", tag )
+  end
 end

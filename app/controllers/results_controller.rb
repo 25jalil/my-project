b91a4b4e9b9687168d8f9697b31874ec5e8f985d
25jalil <25gaisarov@gmail.com>
2016-10-31 10:37:38 +0300

    form to general search

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,5 @@
 class ResultsController < ApplicationController
   def index
-    @posts = ThinkingSphinx.search(params[:query])
+    @posts = ThinkingSphinx.search(params[:query], :with => {:created_at => params[:start_date]..params[:end_date]})
   end
 end
